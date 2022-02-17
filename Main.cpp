156cf654d9ac15c2771f770037683a088679d0bd
RomanManachynskyi <roman.manachynskyi@gmail.com>
2022-02-17 09:41:43 +0200

    Added TheFunction

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,13 @@
 #include <cmath>
 using namespace std;
 
+double TheFunction (double x)
+{
+    if(x >= -5 || x < -2)   return -x;
+    if(x >= -2 || x <  2)   return 2;
+    if(x >=  2 || x <= 5)   return x;
+}
+
 
 int main()
 {
